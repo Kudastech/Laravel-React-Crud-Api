d42ac859fa6cab3b894556021d6eb378ee357934
Kudastech <kudastech@gmail.com>
2024-11-26 09:04:04 +0100

    ApI route was created when API was installed

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::apiResource('users', UserController::class)->except('edit');
+    Route::get('logout ', [AuthController::class, 'logout']);
+
+});
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
