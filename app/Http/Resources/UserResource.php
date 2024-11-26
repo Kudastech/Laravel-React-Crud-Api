8f1e5fb7be91036f984c906e9d7153d421c85ec1
Kudastech <kudastech@gmail.com>
2024-11-26 09:14:18 +0100

    WithoutWrapping was added instead of adding it in the AppServiceProvider to Affect all the Resource...It will only affect the UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,13 @@ class UserResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+
+    public function __construct($resource)
+    {
+        parent::__construct($resource);
+        self::withoutWrapping();
+    }
+
     public function toArray(Request $request): array
     {
         return [
